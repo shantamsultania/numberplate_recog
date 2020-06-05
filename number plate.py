892f1cce430d7207f19e897e4c70837e3a4a1e3e
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2020-06-05 07:25:25 +0530

    Add files via upload

---
+++ number plate.py
@@ -0,0 +1,19 @@
+import cv2
+
+cas = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
+
+image = cv2.imread('download.jpg')
+
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+plate_de = cas.detectMultiScale(gray,1.1,5)
+
+for x,y,w,h in plate_de:
+    area = w*h
+    if area > 500:
+        image = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
+
+cv2.imshow("plate detection",image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ numberplate in video.py
@@ -0,0 +1,26 @@
+import cv2
+
+cas = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
+
+# cam = cv2.VideoCapture("name of your video.mp4)
+cam = cv2.VideoCapture(0)
+
+while True:
+    check,image = cam.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    plate_de = cas.detectMultiScale(gray, 1.1, 5)
+
+    for x, y, w, h in plate_de:
+        area = w * h
+        if area > 500:
+            image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow("plate detection", image)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
